b16f818b450537421dec548db98a5494bf1f3303
nguyen huu huan <huan2903>
2023-03-21 00:33:02 +0700

    xong ban dau tien

--- assignment/src/App.tsx
+++ assignment/src/App.tsx
@@ -1,13 +1,11 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import TableCustom from './page/main-display';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import TableCustom from "./page/main-display";
 function App() {
   return (
-
     <div className="App">
-          <TableCustom/>
-
+      <TableCustom />
     </div>
   );
 }
--- assignment/src/page/main-display.tsx
+++ assignment/src/page/main-display.tsx
@@ -1,57 +1,106 @@
-import React, { useEffect } from "react";
-import { Table } from "antd";
+import React, { useEffect, useState } from "react";
+import { Pagination, Table } from "antd";
 import { ColumnsType } from "antd/es/table";
+import { getUser } from "../services/apiReq";
+import { error } from "console";
 
 const TableCustom = () => {
-  interface DataType {
-    key: string;
+  interface User {
     name: string;
-    age: number;
+    user_name: string;
+    thumb_icon: string;
   }
-  const data: DataType[] = [
-    {
-      key: '1',
-      name: 'John Brown',
-      age: 32,
-    },
-    {
-      key: '2',
-      name: 'Jim Green',
-      age: 42,
+  interface tableParam {
+    page: Number;
+    result: Number;
+  }
+  const [listUser, setListUser] = useState<Array<User>>([]);
+  const [tableParams, setTableParams] = useState<tableParam>({
+    page: 1,
+    result: 10,
+  });
+  useEffect(() => {
+    getUser(tableParams.page, tableParams.result)
+      .then((res) => {
+        console.log(res.data.results);
+        let tempArr: Array<User> = [];
+        res.data.results.forEach((element: any) => {
+          let tempUser: User = {
+            name:
+              element.name.first +
+              " " +
+              element.name.last +
+              " " +
+              element.name.title,
+            user_name: element.login.username,
+            thumb_icon: element.picture.thumbnail,
+          };
+          tempArr.push(tempUser);
+        });
+        setListUser(tempArr);
+        console.log("temp", tempArr);
+      })
+      .catch(() => {
+        console.log("loi roi");
+      });
+  }, [tableParams]);
 
-    },
+  const columns: ColumnsType<User> = [
     {
-      key: '3',
-      name: 'Joe Black',
-      age: 32,
-
-    },
-  ];
-  const columns: ColumnsType<DataType> = [
-    {
-      title: 'Name',
-      dataIndex: 'name',
-      key: 'name',
+      title: "Name",
+      dataIndex: "name",
+      key: "name",
       render: (text) => <a>{text}</a>,
     },
     {
-      title: 'Age',
-      dataIndex: 'age',
-      key: 'age',
+      title: "User Name",
+      dataIndex: "user_name",
+      key: "user_name",
     },
     {
-      title: 'Address',
-      dataIndex: 'address',
-      key: 'address',
+      title: "Thumbnail",
+      dataIndex: "thumb_icon",
+      key: "thumb_icon",
+      render: (text) => <img src={text} alt="thumbnail" />,
     },
-
   ];
 
-  return (<>
-   <Table columns={columns} dataSource={data} />
-  </>
- 
-  ) ;
+  const handleChangeTable = (page: Number, pageSize: Number) => {
+    setTableParams({
+      page: page,
+      result: pageSize,
+    });
+  };
+
+  return (
+    <div
+      style={{
+        margin: "30px",
+        // marginTop:"30px",
+        display: "flex",
+        flexDirection: "column",
+        backgroundColor: "white",
+        padding: 20,
+        boxShadow: "0 0 10px 4px #bfbfbf",
+        borderRadius: 5,
+        height: "90vh",
+      }}
+    >
+      <Table
+        columns={columns}
+        dataSource={listUser}
+        scroll={{ y: "calc(100vh - 200px)" }}
+        pagination={false}
+      />
+      <Pagination
+        style={{ marginTop: "10px" }}
+        total={100}
+        onChange={(page, pageSize) => {
+          handleChangeTable(page, pageSize);
+        }}
+      />
+    </div>
+  );
 };
 
 export default TableCustom;
